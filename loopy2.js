984990aa4ba198393dfc9f9105002865b6cbc678
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-09 22:27:58 -0500

    loopy2

---
+++ loopy2.js
@@ -0,0 +1,13 @@
+function loopyLighthouse(range, multiples, words) {
+  for (let x = range[0]; x < range[1] + 1; x++) {
+    if (x % multiples[0] === 0 && x % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (x % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (x % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(x);
+    }
+  }
+}
